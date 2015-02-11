0972a2311832ce88ab66060194bbb92e05139f9c
Brunno Pleffken <brunno.pleffken@outlook.com>
2015-02-11 17:16:10 -0200

    New Master Page

---
+++ views/layouts/Default.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homefront Online</title>
+	<meta charset="utf-8">
+	<!-- Cascading Stylesheet -->
+	<link rel="stylesheet" href="assets/css/main.css">
+	<!-- JavaScript Third-party -->
+	<script src="thirdparty/jquery/jquery.min.js"></script>
+	<!-- JavaScript Libs -->
+	<script src="resources/Application.js"></script>
+	<script src="resources/Notification.js"></script>
+	<script src="resources/UI.js"></script>
+	<script src="resources/Utils.js"></script>
+</head>
+<body>
+
+	<?php echo $this->content ?>
+
+</body>
+</html>
