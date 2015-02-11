6d8c6f08cccbd96d920103a15326657447173968
Brunno Pleffken <brunno.pleffken@outlook.com>
2015-02-11 00:08:08 -0200

    Class Utils.js

---
+++ resources/Utils.js
@@ -0,0 +1,29 @@
+/*
+ * HOMEFRONT ONLINE GAME CLIENT
+ * (c) 2015 - Homefront Online. All rights reserved.
+ * Developed by Brunno Pleffken Hosti
+ *
+ * ../resources/Utils.js
+ */
+
+var Utils;
+
+(function() {
+	"use strict";
+
+	/*
+	 * --------------------------------------------------------------------
+	 * UTILS.JS CONSTRUCTOR
+	 * --------------------------------------------------------------------
+	 */
+	Utils = function() {};
+
+	/*
+	 * --------------------------------------------------------------------
+	 * BUILD URL TO ACCESS GAME API
+	 * --------------------------------------------------------------------
+	 */
+	Utils.serverUrl = function(controller, method){
+		return SETTINGS.server + "/" + controller + "/" + method;
+	};
+}).call(this);
