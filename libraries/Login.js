c7e1cf154e01ec84fa1784f8204f5bfc6749962c
Brunno Pleffken <brunno.pleffken@outlook.com>
2015-02-12 14:12:31 -0200

    Login form validation

--- libraries/Login.js
+++ libraries/Login.js
@@ -1,43 +1,62 @@
+/*
+ * HOMEFRONT ONLINE GAME CLIENT
+ * (c) 2015 - Homefront Online. All rights reserved.
+ * Developed by Brunno Pleffken Hosti
+ *
+ * ../libraries/Login.js
+ */
+
 $(document).ready(function() {
+	"use strict";
+
+	/*
+	 * --------------------------------------------------------------------
+	 * VALIDATE LOGIN FORM AND SUBMIT
+	 * --------------------------------------------------------------------
+	 */
 	$("#loginForm").on("submit", function(event) {
+		var isValid  = Forms.validateForm($(this));
 		var formData = $(this).serialize();
 
-		$.ajax({
-			url: $(this).attr("action"),
-			type: "POST",
-			dataType: "json",
-			data: formData,
-			beforeSend: function() {
-				UI.startLoader();
-			}
-		})
-		.done(function(data) {
-			if(data.status == "ok") {
-				window.location.href = "report/";
-			}
-			else if(data.status == "wrong_password") {
-				Notification.warning(
-					"Login Failed",
-					"Wrong username or password."
-				);
-				UI.endLoader();
-			}
-			else if(data.status == "banned") {
-				Notification.warning(
-					"Login Failed",
-					"You have been banned."
-				);
-				UI.endLoader();
-			}
-		})
-		.fail(function() {
-			Notification.warning(
-				"HTTP Request failed",
-				"Unable to retrieve player information."
-			);
-			UI.endLoader();
-		});
-
 		event.preventDefault();
+
+		if(!isValid) {
+			$.ajax({
+				url: $(this).attr("action"),
+				type: "POST",
+				dataType: "json",
+				data: formData,
+				beforeSend: function() {
+					UI.startLoader();
+				}
+			})
+			.done(function(data) {
+				if(data.status == "ok") {
+					console.log("Form OK");
+					// window.location.href = "report/";
+				}
+				else if(data.status == "wrong_password") {
+					Notification.warning(
+						"Login Failed",
+						"Wrong username or password."
+					);
+					UI.endLoader();
+				}
+				else if(data.status == "banned") {
+					Notification.warning(
+						"Login Failed",
+						"You have been banned."
+					);
+					UI.endLoader();
+				}
+			})
+			.fail(function() {
+				Notification.warning(
+					"HTTP Request failed",
+					"Unable to retrieve player information."
+				);
+				UI.endLoader();
+			});
+		}
 	});
 });
