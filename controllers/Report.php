b45fcf24cd79da85f3925d6a01d5a2d581678ca1
Brunno Pleffken <brunno.pleffken@outlook.com>
2015-02-12 00:35:10 -0200

    Report page

---
+++ controllers/Report.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * HOMEFRONT ONLINE GAME CLIENT
+ * (C) 2014 - Homefront Online. All rights reserved.
+ * Developed by Brunno Pleffken Hosti
+ *
+ * ../controllers/Report.php
+ */
+
+class Report extends Application
+{
+	public function _beforeFilter()
+	{
+		// ...
+	}
+
+	public function Main()
+	{
+		// ...
+	}
+}
+
+?>
