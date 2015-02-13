5feb2124eb7910d365ee04f0baee95815e241663
Brunno Pleffken <brunno.pleffken@outlook.com>
2015-02-13 00:46:42 -0200

    Research page and controller

---
+++ controllers/Research.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * HOMEFRONT ONLINE GAME CLIENT
+ * (C) 2014 - Homefront Online. All rights reserved.
+ * Developed by Brunno Pleffken Hosti
+ *
+ * ../controllers/Research.php
+ */
+
+class Research extends Application
+{
+	public function _beforeFilter()
+	{
+		// ...
+	}
+
+	public function Main()
+	{
+		// ...
+	}
+}
+
+?>
---
+++ libraries/Research.js
@@ -0,0 +1,13 @@
+/*
+ * HOMEFRONT ONLINE GAME CLIENT
+ * (c) 2015 - Homefront Online. All rights reserved.
+ * Developed by Brunno Pleffken Hosti
+ *
+ * ../libraries/Research.js
+ */
+
+$(document).ready(function() {
+	"use strict";
+
+	// ...
+});
