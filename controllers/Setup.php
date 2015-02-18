8ac5893771b0ad8c8bb9f4b793e79f2c62ba16c7
Brunno Pleffken <brunno.pleffken@outlook.com>
2015-02-18 13:33:27 -0200

    Empire Setup controller

---
+++ controllers/Setup.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * HOMEFRONT ONLINE GAME CLIENT
+ * (C) 2014 - Homefront Online. All rights reserved.
+ * Developed by Brunno Pleffken Hosti
+ *
+ * ../controllers/Setup.php
+ */
+
+class Setup extends Application
+{
+	public function _beforeFilter()
+	{
+		// ...
+	}
+
+	public function Main()
+	{
+		// ...
+	}
+}
+
+?>
